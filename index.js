991361f75ba8cb0072f92191b404246ee6259e31
Eng-Shien Wu <engshien.wu@playnext.com>
2017-01-31 17:14:44 -0800

    Remove SteamItem.hash() as it is unstable

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 'use strict';
 
-const Sha = require('sha.js');
 const Util = require('./src/lib/util.js');
 
 const ITEM_PROPS = [
@@ -9,26 +8,11 @@ const ITEM_PROPS = [
     'original_id'
 ];
 
-function itemHash(item) {
-    const sha256 = Sha('sha256');
-
-    if (item.type) {
-        sha256.update(item.type);
-    }
-
-    for (let t in item.tags) {
-        sha256.update(`|${t.category_name || t.category}: ${t.internal_name || t.name}`);
-    }
-
-    return `1|${item.appid}|${item.market_hash_name || item.market_name || item.name}|${sha256.digest('hex')}`;
-}
-
 function itemProps(item) {
     let props = Util.copyProps(item, ITEM_PROPS);
 
     // Owner Steam ID
     props.owner_steamid = item.owner || item.owner_steamid;
-    props.hash = item.hash || itemHash(item);
 
     // Icon URL, larger one is preferred
     props.icon_url = item.icon_url_large || item.icon_url;
@@ -101,7 +85,5 @@ module.exports = {
 
     fungible: function (item) {
         return item.name == (item.market_hash_name || item.market_name);
-    },
-
-    hash: itemHash
+    }
 };
